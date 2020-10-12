f10b1ce538a52ff8fa4c64ccb7e607d65babf065
Mark Goertz <434548@student.fontys.nl>
2020-10-12 18:59:25 +0200

    Recovery of old view files

---
+++ Fontys S2 Project - Car to go/Controllers/CarController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BLL;
+using Fontys_S2_Project___Car_to_go.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_to_go.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly CarCollection _coll;
+
+        public CarController()
+        {
+            _coll = new CarCollection();
+        }
+        public IActionResult Index()
+        {
+            var all = _coll.GetAllCars();
+            var cars = new List<CarViewmodel>();
+
+            foreach (var car in all)
+            {
+                cars.Add(new CarViewmodel
+                {
+                    ID = car.ID,
+                    Seat = car.Seat,
+                    Enginepower = car.Enginepower,
+                    Acceleration = car.Acceleration,
+                    Brandname = car.Brandname,
+                    Cargospace = car.Cargospace,
+                    Modelname = car.Modelname,
+                    RentalPrice = car.RentalPrice,
+                    Transmission = car.Transmission,
+                    Weight = car.Weight,
+                    Fueltype = car.Fueltype,
+                    ImageLink = car.ImageLink
+
+
+                });
+            }
+            return View(cars);
+        }
+
+    }
+}
--- Fontys S2 Project - Car to go/Controllers/HomeController.cs
+++ Fontys S2 Project - Car to go/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using BLL;
 using Fontys_S2_Project___Car_to_go.Models;
 
 namespace Car_to_go.Controllers
@@ -13,43 +12,23 @@ namespace Car_to_go.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly CarCollection _coll;
-
+        
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-            _coll = new CarCollection();
         }
 
         public IActionResult Index()
         {
-            var all = _coll.GetAllCars();
-            var cars = new List<CarViewmodel>();
-
-            foreach (var car in all)
-            {
-                cars.Add(new CarViewmodel
-                {
-                    ID = car.ID,
-                    Seat = car.Seat,
-                    Enginepower = car.Enginepower,
-                    Acceleration = car.Acceleration,
-                    Brandname = car.Brandname,
-                    Cargospace = car.Cargospace,
-                    Modelname = car.Modelname,
-                    RentalPrice = car.RentalPrice,
-                    Transmission = car.Transmission,
-                    Weight = car.Weight,
-                    Fueltype = car.Fueltype,
-                    ImageLink = car.ImageLink
-
-
-                });
-            }
-            return View(cars);
+            return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Location()
+        {
+            return View();
+        }
+
+        public IActionResult NotImplemented()
         {
             return View();
         }
