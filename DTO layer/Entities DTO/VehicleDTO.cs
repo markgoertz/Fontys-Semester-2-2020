600ecda6105a2295de0923e3dd1d37815aa69205
Mark Goertz <434548@student.fontys.nl>
2020-10-12 13:14:55 +0200

    Initial commit

---
+++ DTO layer/Entities DTO/VehicleDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DTO_layer
+{
+    public class Vehicle
+    {
+        public int ID { get; set; }
+        public string Brandname { get; set; }
+        public string Modelname { get; set; }
+        public string Transmission { get; set; }
+        public int Enginepower { get; set; }
+        public int Weight { get; set; }
+        public int Cargospace { get; set; }
+        public int Seat { get; set; }
+        public double RentalPrice { get; set; }
+        public string Fueltype { get; set; }
+        public string ImageLink { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
---
+++ Fontys S2 Project - Car to go/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fontys_S2_Project___Car_to_go.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
