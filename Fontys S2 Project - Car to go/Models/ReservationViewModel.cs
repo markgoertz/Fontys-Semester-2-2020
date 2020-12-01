333f361b438e43471e9a767d66f3fcaa77be57d9
Mark Goertz <434548@student.fontys.nl>
2020-12-01 13:47:48 +0100

    Changend view for date bug

--- Fontys S2 Project - Car to go/Models/ReservationViewModel.cs
+++ Fontys S2 Project - Car to go/Models/ReservationViewModel.cs
@@ -12,20 +12,24 @@ namespace Fontys_S2_Project___Car_to_go.Models
         public string Email { get; set; }
         public int VehicleID { get; set; }
 
+        
+        private DateTime? date;
+
         [Required]
         [DisplayFormat(DataFormatString = "{0:d}")]
         [DataType(DataType.Date)]
-        private DateTime? date;
         public DateTime StartDate
         {
             get { return date ?? DateTime.Today; }
             set { date = value; }
         }
 
+        
+        private DateTime? enddate;
+
         [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}")]
-        private DateTime? enddate;
         public DateTime EndDate
         {
             get { return enddate ?? DateTime.Today.Date; }
