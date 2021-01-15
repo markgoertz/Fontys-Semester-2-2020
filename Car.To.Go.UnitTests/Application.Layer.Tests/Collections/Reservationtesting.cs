76e738019b40c28f2e1d02ad5a01859ab539b9b0
Mark Goertz <434548@student.fontys.nl>
2021-01-15 13:51:46 +0100

    New unittest

---
+++ Car.To.Go.UnitTests/Application.Layer.Tests/Collections/Reservationtesting.cs
@@ -0,0 +1,47 @@
+﻿using BLL.BLL_Services;
+using BLL.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car.To.Go.UnitTests.Application.Layer.Tests.Collections
+{
+    [TestClass]
+    public class ReservationTesting
+    {
+        private readonly ReservationServices reservationServices = new ReservationServices();
+        private readonly Reservation model = new Reservation();
+
+        [TestMethod]
+        public void ChecksIfGivenDateIsLaterThenStartDate()
+        {
+            
+            bool result = reservationServices.IsEndDateGreaterThenStartDate(Mockdata.MockDataReservations.Isgreaterthenstartdate);
+            Assert.AreEqual(false , result);
+        }
+
+        [TestMethod]
+        public void ChecksIfGivenDateIsEarlierThenStartDate()
+        {
+            bool result = reservationServices.IsEndDateGreaterThenStartDate(Mockdata.MockDataReservations.IsNotgreaterthenstartdate);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void ChecksStartDateIsToday()
+        {
+            bool result = reservationServices.CorrectStartDate(Mockdata.MockDataReservations.TodaysDate);
+            Assert.AreEqual(true, result);
+        }
+        [TestMethod]
+        public void ChecksStartDateIsEarlier()
+        {
+            bool result = reservationServices.IsEndDateGreaterThenStartDate(Mockdata.MockDataReservations.EarlierThenToday);
+            Assert.AreEqual(false, result);
+        }
+        
+
+
+    }
+}
---
+++ Car.To.Go.UnitTests/Mockdata/Mockdata reservation.cs
@@ -0,0 +1,40 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car.To.Go.UnitTests.Mockdata
+{
+    public static class MockDataReservations
+    {
+        public static Reservation Isgreaterthenstartdate = new Reservation()
+        {
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Parse("2020-01-20")
+        };
+
+        public static Reservation IsNotgreaterthenstartdate = new Reservation()
+        {
+            StartDate = DateTime.Now,
+            EndDate = DateTime.Parse("2024-01-20")
+        };
+        public static Reservation TodaysDate = new Reservation()
+        {
+            StartDate = DateTime.Now.Date
+           
+        };
+
+        public static Reservation EarlierThenToday = new Reservation()
+        {
+            StartDate = DateTime.Parse("20 June 2000")
+
+        };
+        public static Reservation LaterThenToday = new Reservation()
+        {
+            StartDate = DateTime.FromFileTime(2030-05-10)
+
+        };
+
+
+    }
+}
